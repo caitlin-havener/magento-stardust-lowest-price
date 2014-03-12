ab775276cb8ce54a57e07442505baa312bb93cb6
Caitlin Havener <caitlin-havener@knights.ucf.edu>
2014-03-12 17:08:42 -0400

    playing with sending queries and retrieving data from semantics

--- app/code/local/Stardust/Lowestprice/Helper/Data.php
+++ app/code/local/Stardust/Lowestprice/Helper/Data.php
@@ -3,46 +3,84 @@ class Stardust_Lowestprice_Helper_Data extends Mage_Core_Helper_Abstract
 {
 	//function to cycle through products
 	public function cycleProductCollection(){
-		$collection = Mage::getModel('catalog/product')->getCollection();
+		global $model;
+		$collection = $model->getCollection();
+		//filter only those that are viewable in catalog
+		$requestor = semanticInstantiate();
 		//potential to add filters in the future below
  		//$collection->addAttributeToFilter('type_id', array('eq' => 'simple'));
  		//$collection->addAttributeToFilter('visibility', 4);
+ 		$i = 0;
  		foreach($collection as $product){
-			//$product = Mage::getModel('catalog/product')->loadByAttribute('sku', $sku);
-			echo $i . "th item <br />";
-				// setters calls
-				//$product->setTeinte(trim((string)$record->web_teinte));
-				
-				//SEND PRODUCT TO API SEARCH FUNCTION HERE
+ 			if($i<30){
+				//$product = Mage::getModel('catalog/product')->loadByAttribute('sku', $sku);
+				echo $i . "th item <br />";
+					// setters calls
+					//$product->setTeinte(trim((string)$record->web_teinte));
+					//SEND PRODUCT TO API SEARCH FUNCTION HERE
+					semanticsSearch($product,$requestor);
 
-			try{
-				$product->save();
-			}
-			catch (Exception $e){
-				echo 'error' . $e;
+				try{
+					//$product->save();
+				}
+				catch (Exception $e){
+					echo 'error' . $e;
+				}
+				$i++;
+			}//end if
+			else{
+				die('finished');
 			}
 		}//end foreach
 
 	}
 
-	//from given product does semantics3 api search, returns result
-	private function semanticsSearch(isobject $product){ //replace isobject with something to check if product is object
-		//API Key: SEM31710CB14396C9944D17C297DEF6D9C81
-		//API Secret: MGJlMjljZTRjNWMxZThkNTRlNWJjMzFiNTliNzkwNjM
-		//To install: 'php composer.phar install' (Github: https://github.com/Semantics3/semantics3-php)
-		require('lib/Semantics3.php');
+	private function semanticInstantiate(){
+		require('../lib/Semantics/Semantics3.php');
+		//require as class http://stackoverflow.com/questions/13433050/best-way-to-include-use-php-classes-in-magento
 
-		$key = 'SEM3xxxxxxxxxxxxxxxxxxxxxx';
-		$secret = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
+		$key = '';
+		$secret = '';
 
 		$requestor = new Semantics3_Products($key,$secret);
+		return $requestor;
+	}
 
-		$requestor->products_field("cat_id", 4992);
-		$requestor->products_field("brand", "Toshiba");
+	//from given product does semantics3 api search, returns result
+	private function semanticsSearch(Mage_Catalog_Model_Product $product, $requestor){ //replace isobject with something to check if product is object
+		global $model;
+		$upc = $model->load($product->getId())->getUpc();
+		$sku = $model->load($product->getId())->getSku();
+		$name = $model->load($product->getId())->getName();
+		$manufacturer = $model->load($product->getId())->getAttributeText('manufacturer');
 
-		$results = $requestor->get_products();
+		echo $sku . $upc . $manufacturer . $name;
+		//must be done by brand and upc, whether upc is encapsulated in string or not is irrelevant
+		if(!empty($upc) && !empty($manufacturer)){
+			//echo "<h1>searching UPC and Manufacturer : " . $upc . " and " . $manufacturer . "</h1>";
+			$requestor->products_field("upc", $upc);
+			$requestor->products_field("brand", $manufacturer);
+			$results = json_decode($requestor->get_products(),true);
 
-		echo $results;
+			//echo '<h2>The results for product ' . $product->getId() . ' are </h2>' . $results;
+			if ($results['total_results_count'] > 0){
+				echo '<h2>The results for product ' . $product->getId() . ' are </h2>';
+				print_r($results);
+			}
+		}
+		/*else if (!empty($sku) && !empty($manufacturer)){
+			echo "<h1>searching sku and Manufacturer : " . $sku . " and " . $manufacturer . "</h1>";
+			$requestor->products_field("model", $sku);
+			$requestor->products_field("manufacturer", $manufacturer);
+			$results = $requestor->get_products();
+		}*/
+		//else{
+			//echo "Could not find manufacturer and UPC or manufacturer and sku combination for product ID " . $product->getId();
+	//	}
+		//Must add search by queries in admin config (semantics categories and magento attributes)
+		//brand, model, upc, manufacturer
+		//if($upc != "")
+
 	}
 	//obtains lowest possible price of given product with price and config margin
 	//compares lowest price to lowest possible price, returns difference
